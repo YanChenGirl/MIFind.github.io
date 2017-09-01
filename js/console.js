150968547bd545dcae9fc72d3bc15cbab5982539
coderxuan <mifind@sina.com>
2017-09-01 13:34:39 +0800

    基础组建平台

--- js/console.js
+++ js/console.js
@@ -1,2 +1,3 @@
-console.log('你好，这里是迷路的二十五岁。',"color: red; font-size: 20px");
-console.log("%cHelloweba欢迎您！","color: red; font-size: 20px");
+var str = "你好，这里是MIFind的console。一些技术相关整理！\n这是一条征婚广告和异性交友广告，哈哈哈哈哈请加我微信严肃脸，：%cmystylexuan%c！\n";
+console.log("%c"+str+"\n%c    ","color: #6190e8;","padding: 0 5px;color: #fff;background: #6190e8;font-size:20px;",
+    "color: #6190e8;","background: url(https://github.com/MIFind/MIFind.github.io/blob/master/img/mifind.jpeg) no-repeat left center;font-size: 100px;","\n\n");
