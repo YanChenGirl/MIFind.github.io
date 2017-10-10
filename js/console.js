a3a2161690e8b0c0eac4e98b001db9a4703d59d7
铉嘉伟 <xuanjw@infore.com>
2017-10-10 09:35:55 +0800

    页面改版

--- js/console.js
+++ js/console.js
@@ -1,3 +1,3 @@
-var str = "你好，这里是MIFind的console。一些技术相关整理！\n这是一条征婚广告和异性交友广告，哈哈哈哈哈请加我微信，严肃脸，：%cmystylexuan%c！\n";
+var str = "你好，这里是MIFind的console。能来到这里你就已经比全世界百分之九十五的人出色了。这里是我的一些技术相关整理！\n我的微信：%cmystylexuan%c！\n我的公众号：%cmifind25th%c！\n好了，你已经得到了我的全部联系方式，那么，你想怎么办呢？";
 console.log("%c"+str+"\n%c    ","color: #6190e8;","padding: 0 5px;color: #fff;background: #6190e8;font-size:20px;",
     "color: #6190e8;","background: url('http://mifind.online/img/mifind.jpeg') no-repeat left center;background-size:100%;font-size: 100px;","\n\n");
