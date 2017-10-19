a17d16c93a8bf7241db6a24ed6e245b9b886fc45
铉嘉伟 <xuanjw@infore.com>
2017-10-19 17:39:27 +0800

    两篇文档

--- js/console.js
+++ js/console.js
@@ -1,3 +1,3 @@
-var str = "你好，这里是MIFind的console。能来到这里你就已经比全世界百分之九十五的人出色了，并且你用实际行动向我证明你真的怼我很有意思，哈哈哈哈。这里是我的一些技术相关整理！\n我的微信：%cmystylexuan%c！\n你懂得😉~";
+var str = "你好，这里是MIFind的console。能来到这里你就已经比全世界百分之九十五的人出色了，并且你用实际行动向我证明你真的对我很有意思，哈哈哈哈。这里是我的一些技术相关整理！\n我的微信：%cmystylexuan%c！\n你懂得😉~";
 console.log("%c"+str+"\n%c    ","color: #6190e8;","padding: 0 5px;color: #fff;background: #6190e8;font-size:20px;",
     "color: #6190e8;","background: url('http://mifind.online/img/mifind.jpeg') no-repeat left center;background-size:100%;font-size: 100px;","\n\n");
